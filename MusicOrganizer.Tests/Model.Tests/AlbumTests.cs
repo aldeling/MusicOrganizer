ff979121888ee98fcc5a406ee21f574b69c0db43
Athea DeLing <almdeling@gmail.com>
2022-12-06 22:44:50 -0500

    test 3 fail
    Co-authored by: Hans Ellis <bullhorn@gmail.com>
    Co-authored-by: Greg Ramsower <greg.ramsower@gmail.com>

--- MusicOrganizer.Tests/Model.Tests/AlbumTests.cs
+++ MusicOrganizer.Tests/Model.Tests/AlbumTests.cs
@@ -15,6 +15,7 @@ namespace MusicOrganizer.Test
       Album newAlbum = new Album("test");
       Assert.AreEqual(typeof(Album), newAlbum.GetType());
     }
+
     [TestMethod]
     public void GetAlbumTitle_ReturnsAlbumTitle_String()
     {
@@ -27,6 +28,23 @@ namespace MusicOrganizer.Test
 
       //Assert
       Assert.AreEqual(albumTitle, result);
+    }
+    
+    [TestMethod]
+    public void SetAlbumTitle_SetAlbumTitle_String()
+    {
+    //Arrange
+    string albumTitle = "Great Album!";
+    Album newAlbum = new Album(albumTitle);
+
+    //Act
+    string updatedAlbumTitle = "Grrreat Album!";
+    newAlbum.AlbumTitle = updatedAlbumTitle;
+    string result = newAlbum.AlbumTitle;
+    
+    //Assert
+    Assert.AreEqual(updatedAlbumTitle, result);
+
     }
   }
 }
