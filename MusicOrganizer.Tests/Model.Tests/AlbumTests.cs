473ccd260c75f4769c3212f3b5b04f5cd6c8ff4f
Athea DeLing <almdeling@gmail.com>
2022-12-06 23:30:03 -0500

    test 5 fail
    Co-authored by: Hans Ellis <bullhorn@gmail.com>
    Co-authored-by: Greg Ramsower <greg.ramsower@gmail.com>

--- MusicOrganizer.Tests/Model.Tests/AlbumTests.cs
+++ MusicOrganizer.Tests/Model.Tests/AlbumTests.cs
@@ -62,6 +62,40 @@ namespace MusicOrganizer.Test
       //
       CollectionAssert.AreEqual(newList, result);
     }
+
+    [TestMethod]
+    public void GetAll_ReturnsAlbums_AlbumList()
+    {
+      //Arrange
+      string album01 = "Dark Side of the Moon";
+      string album02 = "Punisher";
+      Album newAlbum1 = new Album(album01);
+      Album newAlbum2 = new Album(album02);
+      List<Album> newList = new List<Album> { newAlbum1, newAlbum2 };
+
+      //Act
+      List<Album> result = Album.GetAll();
+
+      //Assert
+      CollectionAssert.AreEqual(newList, result);
+    }
+
+    [TestMethod]
+    public void SetAlbumArtist_SetsAlbumArtist_String()
+    
+    {
+      //Arrange
+      string albumTitle = "Punisher";
+      Album newAlbum = new Album(albumTitle);
+
+      //Act
+      string albumArtist = "Phoebe";
+      newAlbum.AlbumArtist = albumArtist;
+      string result = newAlbum.AlbumArtist;
+
+      //Assert
+      Assert.AreEqual(albumArtist, result);
+
+    }
   }
- 
 }
